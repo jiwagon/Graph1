492922004d70accab0ad6e3902e677638803bfe9
jiwagon <jiwagon11@gmail.com>
2023-04-19 09:56:00 -0400

    created addEdge method

--- src/Graph.java
+++ src/Graph.java
@@ -18,4 +18,37 @@ public class Graph {
             this.vertices.add(toAdd);
         }
     }
+
+    public void addEdge(String startVer, String endVer, double wt) {
+
+        // check if start vertex exists or not
+        int startPos = this.getIndex();
+        if (startPos < 0) {
+            this.addVertex(startVer);
+        }
+        // check if end vertex exists or not
+        int endPos = this.getIndex(endVer);
+        if (endPos < 0) {
+            this.addVertex(endVer);
+        }
+        Vertex start = this.vertices.get(startPos);
+        boolean exists = false;
+        for (int i = 0; i < start.edges.size(); i++) {
+            if (start.edges.get(i).eVertex.vName == endVer) {
+                    exists = true;
+                    System.out.println("Edge already exists in Graph");
+                    return;
+                }
+            }
+            // Add same edge from end to start
+            Edge edge =  new Edge(new Vertex(endVer), wt);
+            start.edges.add(edge);
+        // add vertex to startVertex arraylist of edges if edge with endVertex is not existing
+        // add vertex to endVertex arraylist of edges if edge with startVertex is not existing
+    }
+
+
+    public int getIndex() {
+
+    }
 }
